84159515b1882aa4eae540cb185f88f65c3f804b
jmasterfunk84 <48972964+jmasterfunk84@users.noreply.github.com>
2024-01-04 01:54:48 -0600

    typeessss

--- src/smsf/nsmsf-handler.c
+++ src/smsf/nsmsf-handler.c
@@ -230,7 +230,7 @@ bool smsf_nsmsf_sm_service_handle_uplink_sms(
             ogs_log_hexdump(OGS_LOG_INFO, outbuf, 15);
 
             ogs_info("I am [%s]", smsf_ue->gpsi);
-            smsf_ue_t mt_smsf_ue;
+            smsf_ue_t *mt_smsf_ue = NULL;
             mt_smsf_ue = smsf_ue_find_by_gpsi(outbuf);
             if (!mt_smsf_ue)
                 ogs_error("Can't find MT Sub");
