2635136109d564fa58d05d0dc8ec11b96ca62ad9
jmasterfunk84 <48972964+jmasterfunk84@users.noreply.github.com>
2024-01-04 08:49:43 -0600

    format a proper gpsi

--- src/smsf/nsmsf-handler.c
+++ src/smsf/nsmsf-handler.c
@@ -231,7 +231,9 @@ bool smsf_nsmsf_sm_service_handle_uplink_sms(
 
             ogs_info("I am [%s]", smsf_ue->gpsi);
             smsf_ue_t *mt_smsf_ue = NULL;
-            mt_smsf_ue = smsf_ue_find_by_gpsi(outbuf);
+            char *mt_gpsi = ogs_msprintf("msisdn-%s",outbuf);
+            ogs_info("Looking for [%s]", mt_gpsi);
+            mt_smsf_ue = smsf_ue_find_by_gpsi(mt_gpsi);
             if (!mt_smsf_ue)
                 ogs_error("Can't find MT Sub");
 
