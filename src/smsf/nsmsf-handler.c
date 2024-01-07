cfedf91b04347509cc47d8a49ee7b596aab84feb
jmasterfunk84 <48972964+jmasterfunk84@users.noreply.github.com>
2024-01-07 10:32:19 -0600

    successful parse of tpdu for gpsi.  holding back nsmsf response.

--- src/smsf/nsmsf-handler.c
+++ src/smsf/nsmsf-handler.c
@@ -98,9 +98,6 @@ int smsf_nsmsf_sm_service_handle_deactivate(
 
     ogs_info("Deactivate");
 
-    // This might be too soon.
-    smsf_ue_remove(smsf_ue);
-
     ogs_sbi_message_t sendmsg;
     ogs_sbi_header_t header;
     ogs_sbi_response_t *response = NULL;
@@ -117,6 +114,9 @@ int smsf_nsmsf_sm_service_handle_deactivate(
     response = ogs_sbi_build_response(&sendmsg, OGS_SBI_HTTP_STATUS_NO_CONTENT);
     ogs_assert(response);
     ogs_assert(true == ogs_sbi_server_send_response(stream, response));
+
+    // This might be too soon.  Maybe some state machine work to do, too.
+    smsf_ue_remove(smsf_ue);
 
     return OGS_OK;
 }
@@ -244,9 +244,9 @@ bool smsf_nsmsf_sm_service_handle_uplink_sms(
 
             case 1:
                 ogs_info("SMS-SUBMIT (ms->n)");
-                
+
                 smsf_sms_tpdu_submit_t tpdu_submit;
-                memcpy(&tpdu_submit.header, sms_payload_buf->data, 2);
+                memcpy(&tpdu_submit, sms_payload_buf->data, 2);
                 ogs_pkbuf_pull(sms_payload_buf, 2);
                 memcpy(&templen, sms_payload_buf->data, 1);
                 memset(&tpdu_submit.tp_destination_address, 0, sizeof(smsf_sms_tp_address_t));
@@ -353,32 +353,32 @@ bool smsf_nsmsf_sm_service_handle_uplink_sms(
     /* if UE found, then say so.  If ue not found, respond with error. */
     /* form the SmsRecordDeliveryData */
 
-    ogs_sbi_message_t sendmsg;
-    ogs_sbi_header_t header;
-    ogs_sbi_response_t *response = NULL;
+    // ogs_sbi_message_t sendmsg;
+    // ogs_sbi_header_t header;
+    // ogs_sbi_response_t *response = NULL;
 
-    OpenAPI_sms_record_delivery_data_t SmsRecordDeliveryData;
+    // OpenAPI_sms_record_delivery_data_t SmsRecordDeliveryData;
 
-    memset(&SmsRecordDeliveryData, 0, sizeof(SmsRecordDeliveryData));
+    // memset(&SmsRecordDeliveryData, 0, sizeof(SmsRecordDeliveryData));
 
-    SmsRecordDeliveryData.sms_record_id = SmsRecordData->sms_record_id;
-    SmsRecordDeliveryData.delivery_status = 
-            OpenAPI_sms_delivery_status_SMS_DELIVERY_SMSF_ACCEPTED;
+    // SmsRecordDeliveryData.sms_record_id = SmsRecordData->sms_record_id;
+    // SmsRecordDeliveryData.delivery_status = 
+    //         OpenAPI_sms_delivery_status_SMS_DELIVERY_SMSF_ACCEPTED;
 
-    memset(&header, 0, sizeof(header));
-    header.service.name =
-        (char *)OGS_SBI_SERVICE_NAME_NSMSF_SMS;
-    header.api.version = (char *)OGS_SBI_API_V2;
-    header.resource.component[0] = (char *)OGS_SBI_RESOURCE_NAME_UE_CONTEXTS;
-    header.resource.component[1] = smsf_ue->supi;
-    header.resource.component[2] = (char *)OGS_SBI_RESOURCE_NAME_SEND_SMS;
+    // memset(&header, 0, sizeof(header));
+    // header.service.name =
+    //     (char *)OGS_SBI_SERVICE_NAME_NSMSF_SMS;
+    // header.api.version = (char *)OGS_SBI_API_V2;
+    // header.resource.component[0] = (char *)OGS_SBI_RESOURCE_NAME_UE_CONTEXTS;
+    // header.resource.component[1] = smsf_ue->supi;
+    // header.resource.component[2] = (char *)OGS_SBI_RESOURCE_NAME_SEND_SMS;
 
-    memset(&sendmsg, 0, sizeof(sendmsg));
-    sendmsg.SmsRecordDeliveryData = &SmsRecordDeliveryData;
+    // memset(&sendmsg, 0, sizeof(sendmsg));
+    // sendmsg.SmsRecordDeliveryData = &SmsRecordDeliveryData;
 
-    response = ogs_sbi_build_response(&sendmsg, OGS_SBI_HTTP_STATUS_OK);
-    ogs_assert(response);
-    ogs_assert(true == ogs_sbi_server_send_response(stream, response));
+    // response = ogs_sbi_build_response(&sendmsg, OGS_SBI_HTTP_STATUS_OK);
+    // ogs_assert(response);
+    // ogs_assert(true == ogs_sbi_server_send_response(stream, response));
 
     /* need xact and related stream, because your stream disappears) */
 
