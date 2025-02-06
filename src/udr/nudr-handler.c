5bf1c02fff74c306ff305ccaabe1e981befeb87e
jmasterfunk84 <48972964+jmasterfunk84@users.noreply.github.com>
2025-02-05 20:46:50 -0600

    free ambr only if it was populated.

--- src/udr/nudr-handler.c
+++ src/udr/nudr-handler.c
@@ -519,6 +519,8 @@ bool udr_nudr_dr_handle_subscription_provisioned(
                 AccessAndMobilitySubscriptionData.gpsis = GpsiList;
         }
 
+        memset(&SubscribedUeAmbr, 0, sizeof(SubscribedUeAmbr));
+
         if (amdatamask & OGS_SBI_AM_DATA_FIELDS_SUBSCRIBED_UE_AMBR) {
             SubscribedUeAmbr.uplink = ogs_sbi_bitrate_to_string(
                     subscription_data.ambr.uplink, OGS_SBI_BITRATE_KBPS);
@@ -594,8 +596,10 @@ bool udr_nudr_dr_handle_subscription_provisioned(
         }
         OpenAPI_list_free(GpsiList);
 
-        ogs_free(SubscribedUeAmbr.uplink);
-        ogs_free(SubscribedUeAmbr.downlink);
+        if (SubscribedUeAmbr.uplink)
+            ogs_free(SubscribedUeAmbr.uplink);
+        if (SubscribedUeAmbr.downlink)
+            ogs_free(SubscribedUeAmbr.downlink);
 
         OpenAPI_list_for_each(DefaultSingleNssaiList, node) {
             OpenAPI_snssai_t *Snssai = node->data;
